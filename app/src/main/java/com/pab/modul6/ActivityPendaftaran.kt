31d84816fc96a766fd9d6894411a7796a94fb273
dika ramadani <dikabangkok21@gmail.com>
2025-12-07 16:59:29 +0700

    android m6

--- app/src/main/java/com/pab/modul6/ActivityPendaftaran.kt
+++ app/src/main/java/com/pab/modul6/ActivityPendaftaran.kt
@@ -1,6 +1,5 @@
 package com.pab.modul5_view
 
-import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
@@ -8,28 +7,21 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class ActivityPendaftaran : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_pendaftaran)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        val btnMasuk = findViewById<Button>(R.id.btn_masuk)
-        val btnDaftar = findViewById<Button>(R.id.btn_daftar)
+        val btnKembali = findViewById<Button>(R.id.btnKembali)
 
-        btnMasuk.setOnClickListener {
-            val intent = Intent(this, DashboardActivity::class.java)
-            startActivity(intent)
-        }
-
-        btnDaftar.setOnClickListener {
-            val intent = Intent(this, ActivityPendaftaran::class.java)
-            startActivity(intent)
+        btnKembali.setOnClickListener {
+            finish()
         }
     }
 }
--- app/src/main/java/com/pab/modul6/MainActivity.kt
+++ app/src/main/java/com/pab/modul6/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.pab.modul5_view
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
@@ -7,21 +8,28 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class ActivityPendaftaran : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_pendaftaran)
+        setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        val btnKembali = findViewById<Button>(R.id.btn_kembali)
+        val btnMasuk = findViewById<Button>(R.id.btnMasuk)
+        val btnDaftar = findViewById<Button>(R.id.btnDaftar)
 
-        btnKembali.setOnClickListener {
-            finish()
+        btnMasuk.setOnClickListener {
+            val intent = Intent(this, DashboardActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnDaftar.setOnClickListener {
+            val intent = Intent(this, ActivityPendaftaran::class.java)
+            startActivity(intent)
         }
     }
 }
